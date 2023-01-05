2a22fb215c4564c55aaddd73041d429121d8e680
rakeshkumarinstantsys <117158001+rakeshkumarinstantsys@users.noreply.github.com>
2023-01-05 11:17:35 +0530

    Update FactorlabLogin.java

--- TestLoginDetails/src/test/java/com/Factorlab/Tests/FactorlabLogin.java
+++ TestLoginDetails/src/test/java/com/Factorlab/Tests/FactorlabLogin.java
@@ -105,7 +105,7 @@ public class FactorlabLogin {
 	
 	@Test
 	public void Testsecond() {
-		System.out.("This is just a sample text message for 2nd test");
+		System.out.println("This is just a sample text message for 2nd test");
 				
 	}
 	
